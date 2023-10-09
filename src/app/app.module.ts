0e95ca7b8c1fc023f762f14f207dc43d66b8019d
Rani-Bhandary <rani.b1451@gmail.com>
2023-10-09 13:38:11 +0530

    First Version

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { ShowStudentsComponent } from './show-students/show-students.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddStudentComponent,
+    ShowStudentsComponent,
+    StudentDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
